7a328b2ade3479de53e92937b7e0eb329b98b116
shayth1 <shaythwork@gmail.com>
2020-12-29 19:27:38 +0200

    add lab03

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 'use strict';
 var Name = prompt("What's your name?").toLowerCase();
 alert("Welcome " + Name);
-confirm("Please answer with y/n");
+confirm("answer with y/n");
+
+// score:
+var score = 0;
 
 var Qus1 = prompt("my age is 30? y/n");
 if (Qus1.toLowerCase() === "y") {
@@ -10,21 +13,25 @@ if (Qus1.toLowerCase() === "y") {
 } else {
 
     alert("thats right");
+    score++;
 }
 
 var Qus2 = prompt("im a smoker? y/n");
 if (Qus2.toLowerCase() === "n") {
 
-    alert("i wish that!");
+    alert("i wish that");
 } else {
 
-    alert(" thats right unfortuntly");
+    alert("thats right unfortuntly");
+    score++;
+
 }
 
 var Qus3 = prompt("im a php developer? y/n");
 if (Qus3.toLowerCase() === "y") {
 
-    alert("You are right!");
+    alert("You are right");
+    score++;
 } else {
 
     alert("actuly yes");
@@ -33,7 +40,8 @@ if (Qus3.toLowerCase() === "y") {
 var Qus4 = prompt("im a jordanian? y/n");
 if (Qus4.toLowerCase() === "y") {
 
-    alert("You are right!");
+    alert("You are right");
+    score++;
 } else {
 
     alert("oops thats not right");
@@ -42,10 +50,68 @@ if (Qus4.toLowerCase() === "y") {
 var Qus5 = prompt("i like camping? y/n");
 if (Qus5.toLowerCase() === "y") {
 
-    alert("You are right!");
+    alert("You are right");
+    score++;
 } else {
 
-    alert("Not correct :(");
+    alert("oops thats not right");
+}
+//family array:
+// var family = ["saif","zaid","raad","ahmed"];
+// var Qus6 = prompt("guess my lucky number note that you have only 4 attempts");
+// if (Qus6 == lucky) {
+
+
+var lucky = 100;
+for (var attempt = 1; attempt <= 4; attempt++) {
+    var Qus6 = prompt('guess my lucky number note that you have only 4 attempts');
+    if (parseInt(Qus6) === lucky) {
+        alert("You are right");
+
+        score++;
+
+        break;
+    } else if (parseInt(Qus6) > lucky) {
+        alert("too high");
+        console.log("too high");
+
+    } else if (parseInt(Qus6) < lucky) {
+        alert('too low');
+
+    } else {
+        alert("lets try one more time");
+    }
+    if (attempt === 4) {
+        alert("sorry all of your attempts have been exhausted my lucky number is  " + lucky);
+    }
 }
 
+var family = ["saif", "zaid", "ahmad", "raad"];
+for (tryy = 1; tryy <= 6; tryy++) {
+    var Qus7 = prompt('try to guess a name for one of my male family?').toLowerCase();
+    var mname;
+    for (var hent = 0; hent < family.length; hent++) {
+        if (family[hent] === Qus7) {
+            mname = true;
+            break;
+        } else { mname = false; }
+    }
+    if (mname) {
+        alert('yes he is one of them');
+        score++;
+        break;
+    } else {
+        alert('no he is not one of them');
+    }
+    if (tryy === 6) {
+        var shayth = 'my family male are: ';
+        for (var x = 0; x < family.length; x++) {
+            shayth = shayth + ', ' + family[x];
+        }
+        alert(shayth);
+    }
+}
+
+
+alert("Your Score of 7 is " + score);
 alert("Thank you " + Name); 
